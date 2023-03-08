a1fc46ab89cb0e2497445ac953f2ee1e073feae4
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-03-08 19:28:43 +0330

    first commit

---
+++ app/src/main/java/tayeb/shahbakhsh/tamiratkif/MainActivity.kt
@@ -0,0 +1,11 @@
+package tayeb.shahbakhsh.tamiratkif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
